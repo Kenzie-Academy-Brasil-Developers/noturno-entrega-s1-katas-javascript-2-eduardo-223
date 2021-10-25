a098f56cbeb493c55c909e6ff845c3f66e7107e3
Eduardo Rangel <eduardo_rp23@hotmail.com>
2021-10-24 22:14:00 -0300

    Atualiza o Katas2.js

--- src/katas2.js
+++ src/katas2.js
@@ -1,36 +1,58 @@
-// comece a criar a sua função add na linha abaixo
+function add(numero1, numero2){
 
-// descomente a linha seguinte para testar sua função
-// console.assert(add(3, 5) === 8, 'A função add não está funcionando como esperado');
+    return numero1 + numero2;
+}
 
+console.assert(add(3, 5) === 8, 'A função add não está funcionando como esperado');
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-// comece a criar a sua função multiply na linha abaixo
+function multiply(numero1, numero2){
+    let multiplicacao = 0;
 
+    for(let contador = 1; contador <= numero2; contador++){
+        multiplicacao = add(numero1, multiplicacao);
+    }
 
-// descomente a linha seguinte para testar sua função
-// console.assert(multiply(4, 6) === 24, 'A função multiply não está funcionando como esperado');
+    return multiplicacao;
+}
+console.assert(multiply(4, 6) === 24, 'A função multiply não está funcionando como esperado');
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+function power(numero1, numero2){
+    let exponenciacao = numero1;
 
-// comece a criar a sua função power na linha abaixo
+    for( let contador = 1; contador < numero2; contador++){
+        exponenciacao = multiply(numero1, exponenciacao);
+    }
 
+    return exponenciacao;
+}
+console.assert(power(3, 4) === 81, 'A função power não está funcionando como esperado');
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-// descomente a linha seguinte para testar sua função
-// console.assert(power(3, 4) === 81, 'A função power não está funcionando como esperado');
+function factorial(numero){
+    let fatorial = 1;
 
+    for(let contador = 1; contador <= numero; contador++){
+        fatorial = multiply(fatorial,contador);
+    }
 
-// comece a criar a sua função factorial na linha abaixo
+    return fatorial;
+}
+console.assert(factorial(5) === 120, 'A função factorial não está funcionando como esperado');
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+function fibonacci (numero){
+    let fibo, numeroAux = 1, numeroAux1 = 0,index = 0;
 
-// descomente a linha seguinte para testar sua função
-// console.assert(factorial(5) === 120, 'A função factorial não está funcionando como esperado');
+   while(index < numero-1){
+       fibo = add(numeroAux,numeroAux1);
+       numeroAux1 = numeroAux;
+       numeroAux = fibo;
+       index++;
+    }
+    return fibo;
 
+}
 
-/**
- * BONUS (aviso: o grau de dificuldade é bem maior !!!)
- */
-
-// crie a função fibonacci
-
-
-// descomente a linha seguinte para testar sua função
-// console.assert(fibonacci(8) === 13, 'A função fibonacci não está funcionando como esperado');
+console.assert(fibonacci(8) === 13, 'A função fibonacci não está funcionando como esperado');
